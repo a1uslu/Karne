edb08f1c7de58e5e12feb142686c8d0b53fd7831
a1uslu <a1uslu@icloud.com>
2023-11-10 00:27:16 +0300

    Initial Commit

---
+++ GitKarne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitKarne
+//
+//  Created by Aydın Uslu on 10.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
